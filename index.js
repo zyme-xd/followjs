1e77df1e0eda7b9291ba82a49e588bf941e1cdb5
zyme <lolrichyisswag@gmail.com>
2020-12-31 15:45:16 -0600

    this has a block thing, but it spits an error

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ main()
  for(i=0; time > i; i++){
    if(i < time){
     subid = initial_ids.splice(0,99)
-    await delay(60000)
+    await delay(10000)
+    // await delay(60000)
     info.push(await get('users/lookup', {user_id: subid}))
    } else if(i ==  time){
     subid = initial_ids.splice(0,initial_ids.length)
@@ -27,13 +28,22 @@ main()
  info = info.flat()
  console.log(info)
  while(true){
-  await delay(120000)
+   await delay(10000)
+  // await delay(120000)
    await compare()
  }
 }
 async function get(endpoint, options){
   return new Promise((res, rej) => T.get(endpoint, options, function(err, data, response){
     if (err) {
+      rej(err)
+    }
+    res(data)
+ }))
+}
+async function post(endpoint, options){
+  return new Promise((res, rej) => T.post(endpoint, options, function(err, data, response){
+     if (!err.code == 162){
       rej(err)
     }
     res(data)
@@ -57,32 +67,32 @@ async function getid(){
   return ids
 }
 async function compare(){
-  let reason = 'Unfollowed'
+   global.reason = 'Unfollowed'
     let new_ids = await getid()
     console.log('fetched new ids')
     for(i=0; info.length>i; i++){
       // if ur reading this i need to finish writing this for loop i took this pic b4 i finished this
       if(new_ids.indexOf(info[i].id_str) == -1){
        let data = await get('users/lookup', {user_id: info[i].id_str})
-       T.post('friendships/create.json', {user_id: data[0].id_str}, function(err, data, response){
-         if(errors[0].code == 162){
-            reason = 'Blocked'
-         }
-       })
           console.log(data) 
           if(data[0].id_str == undefined){
              reason = 'Suspended'
              data[0].screen_name = info[i].screen_name
              data[0].name = info[i].name
              data[0].id_str = info[i].id_str
-          } else if(data[0].friends_count == 0){
-            reason = 'Unfollowed All/Locked'
-          }
-          T.post('direct_messages/events/new', {event:{type: 'message_create', message_create: {target:{recipient_id: `${userid}`}, message_data:{text: `userid: ${data[0].id_str} \n handle: @${data[0].screen_name} \n  Reason: ${reason}`}}}})
+            }else if(data[0].friends_count == 0){
+              reason = 'Unfollowed All/Locked'
+            } 
+           let funkyinfo = await post('friendships/create', {user_id: data[0].id_str})
+            if(funkyinfo){
+              reason = 'Blocked'
+            }
+            await post('direct_messages/events/new', {event:{type: 'message_create', message_create: {target:{recipient_id: `${userid}`}, message_data:{text: `userid: ${data[0].id_str} \n handle: @${data[0].screen_name} \n  Reason: ${reason}`}}}})
           console.log(`userid: ${data[0].id_str} \n handle: ${data[0].name} \n nickname: ${data[0].screen_name}`)
+          } 
       }
+      initial_ids = new_ids
+      info = [] // huh
+      
+     await main()
     }
-    initial_ids = new_ids
-    info = [] // huh
-   await main()
-}
