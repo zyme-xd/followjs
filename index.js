3fb3d63c11efb0147b887d4251797e9f859f727e
zyme <lolrichyisswag@gmail.com>
2021-01-10 02:28:06 -0600

    ok now it ACTUALLY Works

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ async function main() {
   for (i = 0; time > i; i++) {
     if (i < time) {
       subid = initial_ids.splice(0, 99)
-      await delay(120000)
+      await delay(10000)
+      // await delay(120000)
       info.push(await get('users/lookup', {user_id: subid}))
     } else if (i == time) {
       subid = initial_ids.splice(0, initial_ids.length)
@@ -27,7 +28,8 @@ async function main() {
   info = info.flat()
   console.log(info)
   while (true) {
-    await delay(180000)
+    await delay(10000)
+    // await delay(180000)
     await compare()
   }
 }
@@ -63,7 +65,8 @@ async function getid() {
   console.log(data.ids.length)
   let ids = data.ids
   while (data.next_cursor) {
-    await delay(60000)
+    await delay(15000)
+    // await delay(60000)
     data = await get('followers/ids', {screen_name: `${user}`,stringify_ids: true,cursor: data.next_cursor_str})
     console.log('fetching more')
     console.log(data.ids.length)
@@ -86,26 +89,24 @@ async function compare() {
         console.log('huh')
         reason = 'Unfollowed All/Locked'
       }
-      async function block() {
-        let funkyinfo = await postalt('friendships/create', {user_id: data[0].id_str})
-        if (funkyinfo.errors == 162) {
-          console.log('wocky slush')
-          reason = 'Blocked'
-        }
+        
+      try {let funkyinfo = await postalt('friendships/create', {user_id: data[0].id_str})
+      if (funkyinfo.errors[0].code == 162) {
+        console.log('wocky slush')
+        reason = 'Blocked'
+      }} catch (error) {console.log('ya they aint block u or sum')}
+      try { let suscheck = await postalt('friendships/create', {user_id: info[i].id_str})
+      if (suscheck.errors[0].code == 108) {
+        console.log('when imposter sus')
+        reason = 'Suspended'
+        data[0].screen_name = info[i].screen_name
+        data[0].name = info[i].name
+        data[0].id_str = info[i].id_str
+    }} catch (error) {console.log('they aint suspended')}
+       function dm(){
+          post('direct_messages/events/new', {event: {type: 'message_create',message_create: {target: {recipient_id: `${userid}`},message_data: {text: `userid: ${data[0].id_str} \n handle: @${data[0].screen_name} \n  Reason: ${reason}`}}}})
       }
-      async function sus() {
-        let suscheck = await postalt('friendships/create', {user_id: info[i].id_str})
-        if (suscheck.errors[0].code == 108) {
-          console.log('when imposter sus')
-          reason = 'Suspended'
-          data[0].screen_name = info[i].screen_name
-          data[0].name = info[i].name
-          data[0].id_str = info[i].id_str
-        }
-      }
-      try {block()} catch (error) {console.log('ya they aint block u or sum')}
-      try {sus()} catch (error) {console.log('they aint suspended')}
-      await post('direct_messages/events/new', {event: {type: 'message_create',message_create: {target: {recipient_id: `${userid}`},message_data: {text: `userid: ${data[0].id_str} \n handle: @${data[0].screen_name} \n  Reason: ${reason}`}}}})
+      dm()
       console.log(`userid: ${data[0].id_str} \n handle: ${data[0].name} \n nickname: ${data[0].screen_name}`)}}
   reason = 'Unfollowed'
   initial_ids = new_ids
